088d905dcdbe05a629e4df6b3abcf18c7c935844
Grizordin <91417760+Grizordin@users.noreply.github.com>
2025-10-28 15:51:16 +0500

    Update telegram_promo_bot_postgres.py

--- telegram_promo_bot_postgres.py
+++ telegram_promo_bot_postgres.py
@@ -2244,7 +2244,7 @@ async def start_webserver():
 
 async def main():
     ensure_limit_tables()
-    await _commands()
+    await set_commands()
     # запускаем webserver и polling одновременно
     await asyncio.gather(
         start_webserver(),
